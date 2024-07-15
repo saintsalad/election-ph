7e833a4645ad59b8a184f87e55f7ab8a0e28af44
caleadrian <keyladrian7@gmail.com>
2024-07-15 10:45:32 +0800

    Not found page updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,30 +1,32 @@
-function NotFoundPage() {
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  const router = useRouter();
+
   return (
-    <>
-      <div className='min-h-full px-4 py-4  sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8'>
-        <div className='mx-auto max-w-max'>
-          <div className='mt-5'>
-            <div className='flex mt-6'>
-              <p className='text-4xl font-extrabold text-blue600 sm:text-5xl'>
-                404
-              </p>
-              <div className='ml-6'>
-                <div className='pl-6 border-l border-gray500'>
-                  <h2 className='text-3xl font-bold tracking-tight text-gray900 dark:text-white sm:text-4xl'>
-                    Something went wrong! - Etwas ist schief gelaufen !
-                  </h2>
-                  <p className='mt-1 text-lg text-gray500 dark:text-white'>
-                    Please select a topic from the tag cloud above or go back
-                    home
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className='absolute left-1/2 top-1/2 mb-16 -translate-x-1/2 -translate-y-1/2 items-center justify-center text-center'>
+      <span className='bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent'>
+        404
+      </span>
+      <h2 className='font-heading my-2 text-2xl font-bold'>
+        Something&apos;s missing
+      </h2>
+      <p>
+        Sorry, the page you are looking for doesn&apos;t exist or has been
+        moved.
+      </p>
+      <div className='mt-8 flex justify-center gap-2'>
+        <Button onClick={() => router.back()} variant='default' size='lg'>
+          Go back
+        </Button>
+        <Button onClick={() => router.push("/")} variant='ghost' size='lg'>
+          Back to Home
+        </Button>
       </div>
-    </>
+    </div>
   );
 }
-
-export default NotFoundPage;
