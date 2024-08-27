3e08e2b1a20a56d27056bd4d7e403f8358de2c1c
caleadrian <keyladrian7@gmail.com>
2024-08-27 20:18:22 +0800

    Add platform condition to candidate view

--- app/candidate/[id]/page.tsx
+++ app/candidate/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Candidate, CandidateRating } from "@/lib/definitions";
-import { useEffect, useState } from "react";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import CandidateViewMobile from "@/components/custom/candidate-view-mobile";
 import { useQuery } from "react-query";
 import axios from "axios";
@@ -36,32 +36,28 @@ function CandidateViewPage({ params }: { params: { id: string } }) {
 
   const { data: candidateRate } = useCandidateRate(params.id || "");
 
-  // useEffect(() => {
-  //   const candidatesStr = localStorage.getItem("candidates") ?? "";
-  //   if (candidatesStr === "") {
-  //     return;
-  //   }
-  //   const candidates: Candidate[] = JSON.parse(candidatesStr);
-  //   const foo = candidates.filter((item) => item.id == params.id)[0];
-  //   setCandidate(foo);
-  // }, [params.id]);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   return (
     <>
-      <div className='absolute top-0 left-0 w-full min-h-[100vh] sm:hidden'>
-        {isFetching && "Loading ..."}
-        {isError && "Error"}
-        {/* {candidate && <CandidateViewMobile candidate={candidate} />} */}
-      </div>
-
-      <div className='absolute  top-0 sm:pt-[44px] lg:pt-[58px] overflow-y-scroll flex-1 h-full w-full no-scrollbar hidden sm:flex'>
-        {candidate && candidateRate && (
-          <CandidateViewDeskTop
-            candidate={candidate}
-            candidateRate={candidateRate}
-          />
-        )}
-      </div>
+      {isDesktop ? (
+        <div className='absolute  top-0 sm:pt-[44px] lg:pt-[58px] overflow-y-scroll flex-1 h-full w-full no-scrollbar'>
+          {candidate && candidateRate && (
+            <CandidateViewDeskTop
+              candidate={candidate}
+              candidateRate={candidateRate}
+            />
+          )}
+        </div>
+      ) : (
+        <div className='absolute top-0 left-0 w-full min-h-[100vh]'>
+          {isFetching && "Loading ..."}
+          {isError && "Error"}
+          {candidate && candidateRate && (
+            <CandidateViewMobile candidate={candidate} />
+          )}
+        </div>
+      )}
     </>
   );
 }
